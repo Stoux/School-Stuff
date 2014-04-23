cbac1cf7e20569dec16012ad73e863a9fa519086
stoux2 <stoux2@gmail.com>
2014-04-24 00:06:39 +0200

    Revert "VAT shit"
    This reverts commit bbc697762c98ba2a0f533d1a82c63d90da1d9fdd.

--- ECTool/ECTL-Tool/src/ectltool/main/Main.java
+++ ECTool/ECTL-Tool/src/ectltool/main/Main.java
@@ -5,20 +5,10 @@
 package ectltool.main;
 
 import ectltool.presentation.MainFrame;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 
 /**
  *
@@ -26,61 +16,10 @@ import java.util.Map;
  */
 public class Main {
 
-    //PRAGMA table_info(`reportrts_user`);
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        HashMap<String, Integer> sold = new HashMap<>();
-        try {
-            File folder = new File("D:\\log");
-            
-            HashSet<String> names = new HashSet<>();
-            names.add("spy215");
-            names.add("smurphyman112");
-            names.add("OminousWo1f");
-            names.add("Itzpersonal");
-            names.add("AtypicalAbyss");
-            
-            for (File f : folder.listFiles()) {
-                BufferedReader br = new BufferedReader(new FileReader(f));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    
-                        
-                }
-            }
-            if (1 + 1 == 2) return;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        
-        
-        try {
-            Class.forName("org.sqlite.JDBC");
-            Connection connection = null;
-                  // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/Leon/Documents/GitHub/School-Stuff/ECTool/ECTL-Tool/res/ReportRTS.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-            ResultSet rs = statement.executeQuery("SELECT COUNT(*) FROM `reportrts_request` WHERE `mod_id` = 518;");
-            while(rs.next())
-            {
-              // read the result set
-              System.out.println(rs.getString(1));
-            }
-            
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        
-        
-        if (1 + 1 == 2) return;
         try {
             new MainFrame().setVisible(true);
         } catch (Exception e) {
